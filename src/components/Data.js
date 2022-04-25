f9cbbc34c90424543b827f7619e0c496dafaf946
iamthefastone <o.osisiogu@gmail.com>
2022-04-25 09:21:55 +0100

    Setup components

---
+++ src/components/Data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: "Obudu Cattle Ranch",
+        location: "Nigeria",
+        googleMapsUrl: "https://goo.gl/maps/GyFJ7qiBN7doaV8f9",
+        startDate: "25 April, 2022",
+        endDate: "24 May, 2022",
+        description: "Obudu Mountain Resort also known as Obudu Cattle Ranch is one of Africa's finest and most interesting and spectacular tourist destinations. Since 2005, a cable car climbing 870 metres (2,850 ft) from the base to the top of the plateau gives visitors a scenic view while bypassing the extremely winding road to the top.",
+        imageUrl: "https://user-images.githubusercontent.com/100476375/165047234-9de5d21a-deda-43f2-956c-ec3d8f9b9dc1.jpeg"
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Syndey Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/LWMJWTwkUErCS2ds7",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/ipx6UfSAqKbZFsFz6",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    }
+]
